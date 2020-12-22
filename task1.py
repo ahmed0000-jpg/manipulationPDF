efd8a92fbd057706d354e9c026372b4481ea4324
IT Educator <ahmedbelaiba19952018@gmail.com>
2020-12-22 17:46:25 +0100

    task1 download file from ftp server

--- task1.py
+++ task1.py
@@ -1,13 +1,15 @@
 import ftplib
 import xlrd
 import pandas as pd
+import os
+import shutil
+
 
 FTP_HOST = "talend.ecolotrans.net"
 
 FTP_USER = "talend"
 
 FTP_PASS = "Rand069845"
-
 # connect to the FTP server
 ftp = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS)
 # force UTF-8 encoding
@@ -17,7 +19,6 @@ ftp.cwd('/Preprod/IN/POC_ON_DEMAND/INPUT/ClientInput')
 
 clients = ftp.nlst()
 print(clients)
-
 for client in clients:
     path_racine = '/Preprod/IN/POC_ON_DEMAND/INPUT/ClientInput'
     path_client = path_racine + '/' + client
@@ -26,15 +27,11 @@ for client in clients:
     liste_fichier_client = ftp.nlst(path_client)
     print(liste_fichier_client)
     if(len(liste_fichier_client) != 0):
+        os.mkdir(client)
         for fichier in liste_fichier_client:
             file_name = fichier.split('/')[7]
             print(file_name)
             with open(file_name, "wb") as file:
                 commande = "RETR " + file_name
                 ftp.retrbinary(commande, file.write)
-            #df = pd.read_excel(fichier)
-            #print(df)
-            #workbook = xlrd.open_workbook(file_name)
-            #print("ahmed")
-            #with open(file_name, "wb") as file:
-                #print("ahmed")
+            shutil.move(file_name, client)
